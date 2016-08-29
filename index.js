ca6a425c45b79cbe347d0ad25ead989bbd19d2c3
feizheng <1290657123@qq.com>
2016-08-29 10:41:55 +0800

    finish

---
+++ index.js
@@ -0,0 +1,13 @@
+exports.entries = function() {
+  var files = glob.sync(globPath);
+  var entries = {},
+    entry, dirname, basename;
+
+  for (var i = 0; i < files.length; i++) {
+    entry = files[i];
+    dirname = path.dirname(entry);
+    basename = path.basename(entry, '.js');
+    entries[path.join(dirname, basename)] = './' + entry;
+  }
+  return entries;
+};
